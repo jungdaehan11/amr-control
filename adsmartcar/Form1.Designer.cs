58a3695681378b6093f58b041a5bcead02903edc
정대한 <exemple146@gmail.com>
2026-07-20 13:19:18 +0900

    4주차 USB 유선 통신 완성 - 방향/정지/비상정지 버튼으로 로봇 제어

--- adsmartcar/Form1.Designer.cs
+++ adsmartcar/Form1.Designer.cs
@@ -36,68 +36,74 @@
             btnBackward = new Button();
             lblSensor = new Label();
             panel1 = new Panel();
+            btnConnect = new Button();
+            lblStatus = new Label();
             SuspendLayout();
             // 
             // btnEStop
             // 
-            btnEStop.Location = new Point(23, 30);
+            btnEStop.Location = new Point(23, 45);
             btnEStop.Name = "btnEStop";
             btnEStop.Size = new Size(368, 190);
             btnEStop.TabIndex = 0;
             btnEStop.Text = "비상정지";
             btnEStop.UseVisualStyleBackColor = true;
+            btnEStop.Click += btnEStop_Click;
             // 
             // btnStop
             // 
-            btnStop.Location = new Point(150, 329);
+            btnStop.Location = new Point(151, 340);
             btnStop.Name = "btnStop";
             btnStop.Size = new Size(112, 34);
             btnStop.TabIndex = 1;
             btnStop.Text = "정지";
             btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
             // 
             // btnForward
             // 
-            btnForward.Location = new Point(150, 264);
+            btnForward.Location = new Point(151, 275);
             btnForward.Name = "btnForward";
             btnForward.Size = new Size(112, 34);
             btnForward.TabIndex = 2;
             btnForward.Text = "전진";
             btnForward.UseVisualStyleBackColor = true;
-            btnForward.Click += button2_Click;
+            btnForward.Click += btnForward_Click;
             // 
             // btnLeft
             // 
-            btnLeft.Location = new Point(23, 329);
+            btnLeft.Location = new Point(24, 340);
             btnLeft.Name = "btnLeft";
             btnLeft.Size = new Size(112, 34);
             btnLeft.TabIndex = 3;
             btnLeft.Text = "좌";
             btnLeft.UseVisualStyleBackColor = true;
+            btnLeft.Click += btnLeft_Click;
             // 
             // btnRight
             // 
-            btnRight.Location = new Point(279, 329);
+            btnRight.Location = new Point(280, 340);
             btnRight.Name = "btnRight";
             btnRight.Size = new Size(112, 34);
             btnRight.TabIndex = 4;
             btnRight.Text = "우";
             btnRight.UseVisualStyleBackColor = true;
+            btnRight.Click += btnRight_Click;
             // 
             // btnBackward
             // 
-            btnBackward.Location = new Point(150, 397);
+            btnBackward.Location = new Point(151, 408);
             btnBackward.Name = "btnBackward";
             btnBackward.Size = new Size(112, 34);
             btnBackward.TabIndex = 5;
             btnBackward.Text = "후진";
             btnBackward.UseVisualStyleBackColor = true;
-            btnBackward.Click += button5_Click;
+            btnBackward.Click += btnBackward_Click;
             // 
             // lblSensor
             // 
             lblSensor.AutoSize = true;
-            lblSensor.Location = new Point(417, 30);
+            lblSensor.Location = new Point(417, 45);
             lblSensor.Name = "lblSensor";
             lblSensor.Size = new Size(103, 25);
             lblSensor.TabIndex = 6;
@@ -111,11 +117,32 @@
             panel1.Size = new Size(409, 241);
             panel1.TabIndex = 7;
             // 
+            // btnConnect
+            // 
+            btnConnect.Location = new Point(23, 5);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(112, 34);
+            btnConnect.TabIndex = 8;
+            btnConnect.Text = "연결";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(141, 9);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(90, 25);
+            lblStatus.TabIndex = 9;
+            lblStatus.Text = "연결 안됨";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(838, 489);
+            Controls.Add(lblStatus);
+            Controls.Add(btnConnect);
             Controls.Add(panel1);
             Controls.Add(lblSensor);
             Controls.Add(btnBackward);
@@ -141,5 +168,7 @@
         private Button btnBackward;
         private Label lblSensor;
         private Panel panel1;
+        private Button btnConnect;
+        private Label lblStatus;
     }
 }
--- adsmartcar/Form1.cs
+++ adsmartcar/Form1.cs
@@ -1,7 +1,15 @@
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Drawing;   // Form1_Load에서 안 쓰면 없어도 되지만, 색 지정 등 대비해 둠
+
 namespace adsmartcar
 {
     public partial class Form1 : Form
     {
+        // 클래스 전체에서 쓰는 시리얼 포트 (아두이노 = COM4, 9600bps)
+        private SerialPort port = new SerialPort("COM4", 9600);
+
         public Form1()
         {
             InitializeComponent();
@@ -9,17 +17,55 @@ namespace adsmartcar
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // 지금은 비워둠 (나중에 초기화 코드 자리)
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        // ---- 연결 / 연결 끊기 ----
+        private void btnConnect_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (!port.IsOpen)
+                {
+                    port.Open();
+                    lblStatus.Text = "연결됨";
+                    btnConnect.Text = "연결 끊기";
+                }
+                else
+                {
+                    port.Close();
+                    lblStatus.Text = "연결 안됨";
+                    btnConnect.Text = "연결";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "연결 실패";
+                MessageBox.Show("포트를 열 수 없습니다: " + ex.Message);
+            }
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        // ---- 명령 한 글자 전송 (공통) ----
+        private void Send(char cmd)
         {
-
+            if (port.IsOpen)
+            {
+                port.Write(cmd.ToString());
+            }
+            else
+            {
+                lblStatus.Text = "먼저 연결하세요";
+            }
         }
+
+        // ---- 방향 / 정지 버튼 ----
+        private void btnForward_Click(object sender, EventArgs e) { Send('g'); }
+        private void btnBackward_Click(object sender, EventArgs e) { Send('b'); }
+        private void btnLeft_Click(object sender, EventArgs e) { Send('l'); }
+        private void btnRight_Click(object sender, EventArgs e) { Send('r'); }
+        private void btnStop_Click(object sender, EventArgs e) { Send('s'); }
+
+        // ---- 비상정지 = 정지와 동일한 's' ----
+        private void btnEStop_Click(object sender, EventArgs e) { Send('s'); }
     }
 }
