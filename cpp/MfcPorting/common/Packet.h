dacc8e7ac979e80c499b2eee34d7b90e2aa41362
정대한 <exemple146@gmail.com>
2026-09-11 00:57:01 +0900

    fix c++ cmd 오탈자 수정

--- cpp/MfcPorting/common/Packet.h
+++ cpp/MfcPorting/common/Packet.h
@@ -7,16 +7,16 @@ constexpr uint8_t STX = 0x02;
 constexpr uint8_t ETX = 0x03;
 
 enum Cmd : uint8_t {
-    CMD_MOVE_STOP = 0x10,
-    CMD_MOVE_FWD = 0x11,
-    CMD_MOVE_BACK = 0x12,
-    CMD_MOVE_LEFT = 0x13,
-    CMD_MOVE_RIGHT = 0x14,
+    CMD_MOVE_FWD = 0x10,
+    CMD_MOVE_BACK = 0x11,
+    CMD_MOVE_LEFT = 0x12,
+    CMD_MOVE_RIGHT = 0x13,
+    CMD_MOVE_STOP = 0x14,
     CMD_DIST = 0x20,
     CMD_CURRENT = 0x21,
     CMD_HEARTBEAT = 0x30,
-    CMD_WARN_OBST = 0x40,
-    CMD_WARN_OVER = 0x41,
+    CMD_WARN_ON = 0x40,
+    CMD_WARN_OFF = 0x41,
 };
 
 struct ParseResult {
